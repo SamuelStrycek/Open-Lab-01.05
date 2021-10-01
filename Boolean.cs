263a4eafd99f8f3ea2a18ab28cbd81898db827ab
SamuelStrycek <91180378+SamuelStrycek@users.noreply.github.com>
2021-10-01 09:50:16 +0200

    Add files via upload

---
+++ Boolean.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Open_Lab_01._05
+{
+    public class Boolean
+    {
+        public bool Reverse(bool value)
+        {
+            if (value == true)
+            {
+                return false;
+
+            }
+            else
+            {
+                return true;
+            }
+    }
+}
